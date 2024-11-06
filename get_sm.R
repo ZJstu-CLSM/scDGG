740f4d9a2988aaad5dce201ece1d8ab570506c1a
ZJstu-CLSM <l_junnan@163.com>
2024-11-06 09:48:48 +0800

    get_sm.R

--- get_sm.R
+++ get_sm.R
@@ -6,7 +6,7 @@ suppressMessages(library(Seurat))
 library(corpcor)
 
 
-#加载基因矩阵
+#Load Gene Matrix
 load_matrix_for_GSE <- function(path){
   expr_matrix = read.csv(path,check.names=FALSE, row.names=1)
   expr_matrix = as.matrix(expr_matrix)
@@ -21,19 +21,6 @@ load_pathway <- function(path,name){
 }
 
 
-mat_gene_reduction <- function(mat_gene) {
-  obj <- CreateSeuratObject(counts=mat_gene)
-  obj <- FindVariableFeatures(obj,
-                              selection.method = "vst",
-                              nfeatures = 2000,verbose=F)
-  obj <- ScaleData(obj,verbose=FALSE)
-  obj <- RunPCA(obj, verbose = FALSE)
-
-  mat_gene.reduction = obj@reductions$pca@cell.embeddings
-  #  cell * gene
-  return (mat_gene.reduction)
-}
-
 get_mat_path <- function(mat_gene, paPath, human_pathway_name) {
   gSet = load_pathway(paPath, paste(human_pathway_name,'.gmt',sep=''))
   return (gSet)
@@ -41,13 +28,12 @@ get_mat_path <- function(mat_gene, paPath, human_pathway_name) {
 
 
 get_gene_coexpression_matrix <- function(mat_gene) {
-  # 标准化基因表达数据
+  # Standardized gene expression data
   mat_gene_scaled = scale(mat_gene)
 
-  # 计算基因间的皮尔逊相关系数矩阵
+  # Calculation of Pearson correlation coefficient matrix between genes
   cor_matrix = cor(mat_gene_scaled, method = "pearson")
 
-  print("维度 of 共表达矩阵:")
   print(dim(cor_matrix))
 
   return(cor_matrix)
@@ -56,29 +42,24 @@ get_gene_coexpression_matrix <- function(mat_gene) {
 
 get_coexpression_matrix <- function(expr_matrix, pathway_genes) {
 
-  # 筛选通路内基因
+  # Screening for genes within pathways
   if (is.list(pathway_genes)) {
     pathway_genes <- unlist(pathway_genes)
   }
   pathway_genes <- as.character(pathway_genes)
   print(pathway_genes)
-  # 移除潜在的空格并统一大小写，以确保精确匹配
+  # Remove potential spaces and standardize case to ensure exact matches
   # pathway_genes <- trimws(toupper(pathway_genes))
   pathway_genes <- trimws(pathway_genes)
-  # 将expr_matrix的列名（基因ID）转换为大写
+
   #colnames(expr_matrix) <- toupper(colnames(expr_matrix))
   selected_genes <- intersect(colnames(expr_matrix), pathway_genes)
 
-  # 提取相关基因的表达子集
+  #  Extraction of expression subsets of related genes
   expr_submatrix <- expr_matrix[, selected_genes]
 
-  # 标准化表达值（可选，但有助于提高相关性计算的准确性）
   expr_scaled <- scale(expr_submatrix)
-
-  # 计算基因间的皮尔逊相关系数矩阵
   cor_matrix <- cor(expr_scaled, method = "pearson")
-
-  # 转置并填充对角线及下三角，以确保对称性
   cor_matrix[lower.tri(cor_matrix)] <- t(cor_matrix)[lower.tri(cor_matrix)]
   diag(cor_matrix) <- 1
 
